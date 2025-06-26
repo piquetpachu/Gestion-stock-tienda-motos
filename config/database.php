46eca27ebbcd26dcc6db4cd9e6c3b03cf41eca90
piquetpachu <pachu510121@gmail.com>
2025-06-26 14:17:11 -0300

    estructura de carpetas y variable de entorno

---
+++ config/database.php
@@ -0,0 +1,23 @@
+<?php
+require_once 'cargar_env.php';
+cargarEnv(__DIR__ . '/../.env');
+
+$host = getenv('DB_HOST');
+$db = getenv('DB_NAME');
+$user = getenv('DB_USER');
+$pass = getenv('DB_PASS');
+$charset = getenv('DB_CHARSET');
+
+$dsn = "mysql:host=$host;dbname=$db;charset=$charset";
+$options = [
+    PDO::ATTR_ERRMODE            => PDO::ERRMODE_EXCEPTION,
+    PDO::ATTR_DEFAULT_FETCH_MODE => PDO::FETCH_ASSOC,
+    PDO::ATTR_EMULATE_PREPARES   => false,
+];
+
+try {
+    $pdo = new PDO($dsn, $user, $pass, $options);
+    echo "Conectado correctamente";
+} catch (PDOException $e) {
+    die("Error de conexión: " . $e->getMessage());
+}
