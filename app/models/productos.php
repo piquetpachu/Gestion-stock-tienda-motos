7e9baf9207512f0217d32cdd61d9bdb313d20563
piquetpachu <pachu510121@gmail.com>
2025-07-23 17:05:43 -0300

    Crud de productos y reparacion de bug en registro

--- app/models/productos.php
+++ app/models/productos.php
@@ -19,8 +19,8 @@ function obtenerProductoPorCodigo($pdo, $codigo)
 
 
 function crearProducto($pdo, $datos) {
-    $sql = "INSERT INTO producto (nombre, descripcion, precio_venta, precio_compra, stock, id_proveedor, id_rubro, fecha_alta, activo, stock_minimo)
-            VALUES (?, ?, ?, ?, ?, ?, ?, NOW(), ?, ?)";
+    $sql = "INSERT INTO producto (nombre, descripcion, precio_venta, precio_compra, stock, id_proveedor, id_rubro, fecha_alta, activo, stock_minimo, codigo_barras)
+            VALUES (?, ?, ?, ?, ?, ?, ?, ?, NOW(), ?, ?)";
     $stmt = $pdo->prepare($sql);
     $stmt->execute([
         $datos['nombre'],
@@ -31,7 +31,8 @@ function crearProducto($pdo, $datos) {
         $datos['id_proveedor'],
         $datos['id_rubro'],
         $datos['activo'],
-        $datos['stock_minimo']
+        $datos['stock_minimo'],
+        $datos['codigo_barras']
     ]);
     return $pdo->lastInsertId();
 }
@@ -39,7 +40,7 @@ function crearProducto($pdo, $datos) {
 function actualizarProducto($pdo, $id, $datos) {
     $sql = "UPDATE producto SET
                 nombre = ?, descripcion = ?, precio_venta = ?, precio_compra = ?, stock = ?,
-                id_proveedor = ?, id_rubro = ?, activo = ?, stock_minimo = ?
+                id_proveedor = ?, id_rubro = ?, activo = ?, stock_minimo = ?, codigo_barras = ?
             WHERE id_producto = ?";
     $stmt = $pdo->prepare($sql);
     $stmt->execute([
@@ -52,6 +53,7 @@ function actualizarProducto($pdo, $id, $datos) {
         $datos['id_rubro'],
         $datos['activo'],
         $datos['stock_minimo'],
+        $datos['codigo_barras'],
         $id
     ]);
     return $stmt->rowCount() > 0;
