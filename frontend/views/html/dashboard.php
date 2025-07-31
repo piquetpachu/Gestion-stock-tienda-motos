6ac6f12a8506f2ae8f85b63f93c89cad38fe6d83
Leonardo <jorgeleonardoenrique@gmail.com>
2025-07-30 23:47:57 -0300

    sigo provando

--- frontend/views/html/dashboard.php
+++ frontend/views/html/dashboard.php
@@ -6,7 +6,8 @@
   <meta name="viewport" content="width=device-width, initial-scale=1">
   <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.min.css" rel="stylesheet">
   <link rel="stylesheet" href="<?php echo 'frontend/views/css/style.css'; ?>">
-
+  <link rel="stylesheet" href="views/css/style.css">
+  
   <?php include 'navbar.php'; ?>
 
 </head>
