ae3fa3bce876c24f0ebd684b3952bd13d4d53ed9
Leonard0Enrique <jorgeleonardoenrique@gmail.com>
2025-09-04 20:22:49 -0300

    test añadir rubro 3

--- frontend/views/html/productos.php
+++ frontend/views/html/productos.php
@@ -160,5 +160,6 @@ body.dark-theme .btn-danger {
   <script src="../js/dashboard-proteccion.js"></script>
   <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/js/bootstrap.bundle.min.js"></script>
   <script src="../js/productos.js"></script>
+  <script src="../js/index.js"></script>
 </body>
 </html>
