07de9544bbb235572dfa7c96fe80981534766b15
Leonard0Enrique <jorgeleonardoenrique@gmail.com>
2025-09-10 14:23:12 -0300

    reverificando

--- frontend/views/html/dashboard.php
+++ frontend/views/html/dashboard.php
@@ -5,9 +5,7 @@
   <title>Panel Principal</title>
   <meta name="viewport" content="width=device-width, initial-scale=1">
   <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.min.css" rel="stylesheet">
-  <style>
-
-  </style>
+  <link rel="stylesheet" href="../css/style.css">
 
   <?php include 'navbar.php'; ?>
 
--- frontend/views/html/productos.php
+++ frontend/views/html/productos.php
@@ -5,6 +5,7 @@
   <meta name="viewport" content="width=device-width, initial-scale=1.0"/>
   <title>Gestión de Productos</title>
   <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.min.css" rel="stylesheet" />
+  <link rel="stylesheet" href="../css/style.css">
 
 <!-- Tom Select -->
 <link href="https://cdn.jsdelivr.net/npm/tom-select@2.3.1/dist/css/tom-select.bootstrap5.min.css" rel="stylesheet">
