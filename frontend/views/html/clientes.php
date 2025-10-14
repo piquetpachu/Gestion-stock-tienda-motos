083f0b91ef07c00dee304c88a9a23524cf7344cb
Leonard0Enrique <jorgeleonardoenrique@gmail.com>
2025-10-14 16:36:21 -0300

    ajustes al fondo del tema claro

--- frontend/views/html/clientes.php
+++ frontend/views/html/clientes.php
@@ -7,14 +7,9 @@
   <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.min.css" rel="stylesheet" />
   <link href="https://cdn.jsdelivr.net/npm/tom-select@2.3.1/dist/css/tom-select.bootstrap5.min.css" rel="stylesheet">
   <link rel="stylesheet" href="../css/style.css">
-
-<head>
-
-  <?php include 'navbar.php'; ?>
-
 </head>
-
-<body class="bodyclientes">
+<body>
+  <?php include 'navbar.php'; ?>
 
   <div class="container mt-5">
     <div class="d-flex justify-content-between align-items-center mb-4">
